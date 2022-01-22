4a20d9917d7f5913878172a6837008339ef3e59d
jassu19 <jassu19@hotmail.com>
2022-01-22 15:08:45 -0500

    second Commit

--- src/main/java/first.java
+++ src/main/java/first.java
@@ -1,5 +1,12 @@
 public class first {
     public static void main(String[] args) {
         System.out.println("first commit ");
+        /*
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jassu19/second.git
+git push -u origin main*/
     }
 }
